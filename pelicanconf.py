90ad72c754922ef8a53bd27d22a26814f85f1a6c
yx0906 <midwestdc914@gmail.com>
2018-03-25 11:01:46 +0800

    change back to plugin pelican-ipynb for notebooks

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,11 +9,10 @@ SITETITLE = "Kimi's Blog"
 SITESUBTITLE = 'Developer, Interested in Python & Data Science'
 SITEDESCRIPTION = 'Kimi\'s Thoughts and Writings'
 SITELOGO = '/images/profile.jpg'
-#SITELOGO = SITEURL + '/images/profile.jpg'
-#FAVICON = SITEURL + '/images/favicon.ico'
+# FAVICON = SITEURL + '/images/favicon.ico'
 
 PATH = 'content'
-STATIC_PATHS = ['pages', 'images','extra', 'extra/robots.txt']
+STATIC_PATHS = ['pages', 'images', 'extra', 'notebooks', 'extra/robots.txt']
 
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
@@ -22,33 +21,33 @@ EXTRA_PATH_METADATA = {
 
 THEME = 'pelican-themes/Flex_blue'
 DATE_FORMATS = {'en': '%a %d %b %Y'}
-ARTICLE_PATHS = ['pages']
+ARTICLE_PATHS = ['pages', 'notebooks']
 ARTICLE_URL = ('{slug}.html')
 ARTICLE_SAVE_AS = ('{slug}.html')
 PAGE_URL = ('{slug}.html')
-#PAGE_SAVE_AS = ('{slug}.html')
-#AUTHOR_URL = ('author/{name}/')
-#TAG_URL = ('tag/{name}/')
+# PAGE_SAVE_AS = ('{slug}.html')
+# AUTHOR_URL = ('author/{name}/')
+# TAG_URL = ('tag/{name}/')
 # Display pages list on the top menu
-#DISPLAY_PAGES_ON_MENU = True
+# DISPLAY_PAGES_ON_MENU = True
 
 # Display categories list on the top menu
 DISPLAY_CATEGORIES_ON_MENU = True
 
 # Display categories list as a submenu of the top menu
-#DISPLAY_CATEGORIES_ON_SUBMENU = False
+# DISPLAY_CATEGORIES_ON_SUBMENU = False
 
 # Display the category in the article's info
-#DISPLAY_CATEGORIES_ON_POSTINFO = False
+# DISPLAY_CATEGORIES_ON_POSTINFO = False
 
 # Display the author in the article's info
-#DISPLAY_AUTHOR_ON_POSTINFO = False
+# DISPLAY_AUTHOR_ON_POSTINFO = False
 
 # Display the search form
-#DISPLAY_SEARCH_FORM = False
+# DISPLAY_SEARCH_FORM = False
 
 # Sort pages list by a given attribute
-#PAGES_SORT_ATTRIBUTE (Title)
+# PAGES_SORT_ATTRIBUTE (Title)
 
 # Display the "Fork me on Github" banner
 GITHUB_URL = None
@@ -69,7 +68,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
+# LINKS = (('Pelican', 'http://getpelican.com/'),
 #         ('Python.org', 'http://python.org/'),
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
 #         ('You can modify those links in your config file', '#'),)
@@ -104,15 +103,13 @@ PYGMENTS_STYLE = 'github'
 
 # Plugin
 PLUGIN_PATHS = ['pelican-plugins']
-#PLUGINS = ['assets', 'sitemap', 'gravatar','extract_toc']
-#PLUGINS = ['better_tables']
-PLUGINS = ['neighbors','liquid_tags.youtube','liquid_tags.notebook','liquid_tags.include_code', 'sitemap', 'extract_toc', 'related_posts', 'render_math', 'filetime_from_git']
 MARKUP = ('md', 'ipynb')
-NOTEBOOK_DIR = 'notebooks'
-#MD_EXTENSIONS = ['codehilite(css_class=codehilite)', 'extra', 'toc', 'meta']
+PLUGINS = ['neighbors', 'pelican-ipynb.markup', 'sitemap', 'extract_toc', 'related_posts', 'render_math', 'filetime_from_git']
+# NOTEBOOK_DIR = 'notebooks'
+# MD_EXTENSIONS = ['codehilite(css_class=codehilite)', 'extra', 'toc', 'meta']
 MARKDOWN = {
         'extension_configs': {
-            'markdown.extensions.codehilite' : {'css_class': 'codehilite',
+            'markdown.extensions.codehilite': {'css_class': 'codehilite',
                 #'noclasses': True, 'pygments_style': 'vim'
             },
             'markdown.extensions.extra': {},
